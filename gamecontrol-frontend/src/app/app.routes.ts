8eb9686a9fdcdacd5510ede9a82b817946786daf
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-03-26 22:52:32 -0300

    feat: preenchendo conteudo do template HTML de gamesList

--- gamecontrol-frontend/src/app/app.routes.ts
+++ gamecontrol-frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component'
 import { LoginComponent } from './pages/login/login.component'
 import { RegisterComponent } from './pages/register/register.component';
+import { GamesListComponent } from './pages/gamesList/games.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'games', component: GamesListComponent },
   { path: '**', redirectTo: '' }
 ];
