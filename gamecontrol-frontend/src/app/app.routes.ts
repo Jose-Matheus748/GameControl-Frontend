807ed870979ac0d54d1b636eae41af2c2aec899c
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-03-27 01:00:33 -0300

    feat: adicionando rota games/:id(Path Variable) para o GameComponent

--- gamecontrol-frontend/src/app/app.routes.ts
+++ gamecontrol-frontend/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component'
 import { LoginComponent } from './pages/login/login.component'
 import { RegisterComponent } from './pages/register/register.component';
+import { GameComponent } from './pages/game/game.component';
 import { GamesListComponent } from './pages/gamesList/games.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'games/:id', component: GameComponent },
   { path: 'games', component: GamesListComponent },
   { path: '**', redirectTo: '' }
 ];
