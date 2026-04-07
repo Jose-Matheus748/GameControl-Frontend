9104c29b061ea4198744e308d781fbd6d8ae9b85
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-04-07 13:43:31 -0300

    style: Implementei o icone no ProfileComponent, e aumentei o tamanho do svg padrão de perfil

--- gamecontrol-frontend/src/app/pages/profile/profile.component.ts
+++ gamecontrol-frontend/src/app/pages/profile/profile.component.ts
@@ -5,11 +5,12 @@ import { forkJoin } from 'rxjs';
 import { UsuarioService, Usuario } from '../../services/user.service';
 import { FollowService } from '../../services/follow.service';
 import { PlaylistService, Playlist } from '../../services/playlist.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './profile.component.html'
 })
 export class ProfileComponent implements OnInit {
