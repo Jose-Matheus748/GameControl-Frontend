01e0ee6f1ac87c25328c6aa4558d5769d27c911e
Jose-Matheus748 <josemathaguiar@gmail.com>
2026-04-13 17:30:55 -0300

    Style: Refatorando estilização do header e SearchBar para melhor alinhamento com usuário

--- gamecontrol-frontend/src/app/components/header/header.component.ts
+++ gamecontrol-frontend/src/app/components/header/header.component.ts
@@ -4,11 +4,12 @@ import { AuthService } from '../../services/auth.service';
 import { RouterModule } from '@angular/router';
 import { SearchbarComponent } from '../searchbar/searchbar.component';
 import { Router } from '@angular/router';
+import { LucideUserRound } from '@lucide/angular';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule, SearchbarComponent],
+  imports: [CommonModule, RouterModule, SearchbarComponent, LucideUserRound],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
