c550bff53e83d1db10cbafe6e38b0dce8215b829
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-05-17 22:08:34 -0300

    test: subindo configurações para teste com cypress

--- gamecontrol-frontend/cypress.config.ts
+++ gamecontrol-frontend/cypress.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   allowCypressEnv: false,
 
   e2e: {
---
+++ gamecontrol-frontend/cypress/e2e/homespec.cy.ts
@@ -0,0 +1,5 @@
+describe('tela inicial', () => {
+  it('deve exibir a tela inicial', () => {
+    cy.visit('http://localhost:4200')
+  })
+})
